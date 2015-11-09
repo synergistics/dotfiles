0d8fb3c1a909f4fc52b048e696556959043d732a
synergistics <kingjak678@gmail.com>
2015-11-09 23:35:55 +0000

    Fully commits terminal usage a la vida de la elefante, makes urxvt look better (also in honor of la elefante)

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,8 @@
+# Colorize output, add filetype indicator, human readable file sizes
+alias ls='ls --color -GFh'
+
+# Same as above, but list all files and in long format
+alias ll='ls --color -GFhla'
+
+# Colorize grep
+alias grep='grep --color=auto'
---
+++ zsh/.zsh/exports.zsh
@@ -0,0 +1,8 @@
+# Ensures charset is correct
+export LANG=en_US.UTF-8
+
+export TERM=rxvt-unicode-256color
+
+export MANPAGER='most'
+export EDITOR='vim'
+
